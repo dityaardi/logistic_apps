19b32d09ff99a21bc3c4d50bcc99caa23c52adf4
ferdy <fpittardi@gmail.com>
2024-01-17 20:53:20 +0700

    add: table transaksi

---
+++ database/migrations/2024_01_17_132934_create_transaksi_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->string('no_transaksi')->unique();
+            $table->string('nama_pengajuan');
+            $table->string('nama_admin');
+            $table->unsignedBigInteger('quantity');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id_user')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
